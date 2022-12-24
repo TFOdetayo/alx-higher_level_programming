2bb07561139529466683395e6bf938460017f421
TFOetayo <tayeodetayo@gmail.com>
2022-12-24 04:29:26 -0800

    8-uppercase.pycompleted

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for letr in str:
+        letr = ord(letr)
+        if letr >= 97 and letr <= 122:
+            letr -= 32
+            letr = chr(letr)
+            print("{}".format(letr), end='')
+            print(" ")
